a7e71bed8ca7146cc7b3ac4bbe089cb475e5168e
Micael Willam Costa de Oliveira - 824213069 <824213069@ulife.com.br>
2024-10-03 21:14:31 -0300

    adicionando lista de exercicios prática04

---
+++ PSC-Aula04-Exercicios/classes/projeto02/Turma.java
@@ -0,0 +1,43 @@
+package projeto02;
+public class Turma {
+
+    private String nome;
+    private String curso;
+    private int quantidadeDeAlunos;
+    private int serie;
+
+    public Turma (String n, String c, int q, int s){
+        nome = n;
+        curso = c;
+        quantidadeDeAlunos = q;
+        serie = s;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCurso(){
+        return curso;
+    }
+
+    public int getQuantidadeAlunos(){
+        return quantidadeDeAlunos;
+    }
+
+    public int getSerie(){
+        return serie;
+    }
+
+    public void setNome(String n){
+        nome = n;
+    }
+
+    public void setCurso(String c){
+        curso = c;
+    }
+
+    public void setQuantidadeAlunos(int q){
+        quantidadeDeAlunos = q;
+    }
+}
